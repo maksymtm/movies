a3439c291073771d1c27612465167800d5d7f3ee
maksymtm <maksym.borodulia@tm1.edu.pl>
2022-06-14 14:34:59 +0200

    add model car

---
+++ WebApplication1/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesc { get; set; }
+        public string img { get; set; }
+        public ushort price { get; set; }
+        public bool isFavourite { get; set; }
+        public bool avaliable { get; set; }
+    }
+}
